eb15e2afff687a08936d2b3d558a130b2128c967
AhmedJameel <roarme247@gmail.com>
2024-09-22 21:15:59 +0300

    some cleaning

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from app.models import Task
-from django.contrib.auth.models import User
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = ['id', 'title', 'description', 'is_completed',]
